5ffb13bc4488758a30ce4c8b45bc61f702571dc0
wuxudognxd <wuxudongxd@gmail.com>
2022-01-21 14:35:50 +0800

    feat: add lable for newColumn input

--- src/views/index.vue
+++ src/views/index.vue
@@ -10,7 +10,7 @@ provide(storeFnSymbol, storeFn);
 // add new column
 const modalState = ref(false);
 const columnName = ref("");
-const modalTitle = "Add a column"
+const modalTitle = "Add a column";
 const closeModal = () => {
   modalState.value = false;
   columnName.value = "";
@@ -35,17 +35,22 @@ const closeModal = () => {
           + Add column
         </div>
       </div>
-      <Modal :modal-state="modalState" :modal-title="modalTitle" :close-modal="closeModal">
+      <Modal
+        :modal-state="modalState"
+        :modal-title="modalTitle"
+        :close-modal="closeModal">
         <div class="h-[11rem] w-full p-5">
+          <label for="columnName" class="ml-1 text-gray-700">Column name</label>
           <input
-            class="h-10 w-full pl-4 bg-gray-100 border rounded-md focus:outline-0 focus:border-blue-500 focus:ring focus:bg-white"
+            id="columnName"
+            class="h-10 w-full pl-4 mt-2 bg-gray-100 border rounded-md focus:outline-0 focus:border-blue-500 focus:ring focus:bg-white"
             placeholder="Enter a column name (To Do, In Progress, Done)"
             type="text"
             v-model="columnName" />
           <button
             @click="storeFn.newColumn(columnName), closeModal"
             :disabled="!Boolean(columnName.length)"
-            class="w-32 h-8 mt-10 text-white border border-gray-300 rounded-md bg-green-600 hover:bg-green-700 disabled:bg-green-600/50 transition-all">
+            class="w-32 h-8 mt-8 text-white border border-gray-300 rounded-md bg-green-600 hover:bg-green-700 disabled:bg-green-600/50 transition-all">
             Create column
           </button>
         </div>
