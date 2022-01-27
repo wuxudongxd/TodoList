654c6299ca9f6cd496a6eee62f58f4b1dab3a417
wuxudognxd <wuxudongxd@gmail.com>
2022-01-27 23:08:44 +0800

    deps: add morgan logger

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 import router from "./router";
 
 const app = express();
 
 app.use(cors());
+app.use(morgan("short"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
